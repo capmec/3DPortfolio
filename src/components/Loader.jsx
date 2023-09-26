51c57672c142bf24a7ebd5bf472af1b681eadca6
capmec <capmec@gmail.com>
2023-09-26 23:22:58 +0200

    new loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,22 +1,21 @@
 import { Html, useProgress } from '@react-three/drei'
 
-const Loader = () => {
+const CanvasLoader = () => {
   const { progress } = useProgress()
   return (
-    <Html>
-      <span className='canvas-load'></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: '#f1f1f1',
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
+    <Html
+      as='div'
+      center
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <span className='canvas-loader'></span>
     </Html>
   )
 }
 
-export default Loader
+export default CanvasLoader
